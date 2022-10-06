f762fe4a65b60248f444e56ad7933bb9a1f49acd
JKDow <114895228+JKDow@users.noreply.github.com>
2022-10-06 19:11:49 +1100

    Initial FIles

---
+++ digisafe_c/digisafe_c/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * digisafe_c.cpp
+ *
+ * Created: 6/10/2022 7:11:28 PM
+ * Author : jkdow
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
